e5179b3db45519f7f9d61b55876dd3a4b6b199a8
victorAbastoflor <ayv1009450@est.univalle.edu>
2024-06-16 22:56:47 -0400

    prueba

---
+++ Controllers/AccessController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NurseCourse.Repositories;
+
+namespace NurseCourse.Controllers
+{
+    public class AccessController : Controller
+    {
+        private readonly IAccess _access;
+
+        public AccessController(IAccess iaccess) => _access = iaccess;
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
